f76b1100f64823f2e78981d01542fcb254c9a35e
nino-rgb <pnekoneko907@gmail.com>
2024-10-05 18:04:56 +0900

    アプリケーションサーバーの作成とAPI疎通が取れるところまで作成

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
-const hello: string = "Hello";
-const world: string = "World";
-const message: string = `${hello} ${world}`;
+import express, { Express } from "express";
+import cors from "cors";
+import { AddressInfo } from "net";
 
-console.log(message);
+async function main() {
+  const app: Express = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.disable("x-powerd-by");
+
+  const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
+
+  app.use(cors(corsOptions));
+
+  const sever = app.listen(4000, () => {
+    const address = sever.address() as AddressInfo;
+    console.log("Node.js is listening to PORT:" + address.port);
+  });
+
+  app.get("/", async (req, res) => {
+    res.json("テスト");
+  });
+}
+main();
