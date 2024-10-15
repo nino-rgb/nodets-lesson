c866df1939c808956b59b250dd0fc7e00fcd7b5d
nino-rgb <pnekoneko907@gmail.com>
2024-10-15 16:06:11 +0900

    ファイル分割完

--- src/repositories/interface.ts
+++ src/repositories/interface.ts
@@ -5,5 +5,5 @@ export interface ITodoRepository {
   getById(id: number): Promise<Todo | Error>;
   create(todo: Todo): Promise<number | Error>;
   update(id: number, todo: Todo): Promise<void | Error>;
-  //   delete(id: number): Promise<void | Error>;
+  delete(id: number): Promise<void | Error>;
 }
